bd5aceda880f36e3509fa98b2b8ac6420ba620a1
Bao Dang <dtquocbao@gmail.com>
2020-06-10 07:37:14 -0700

    init

---
+++ includes/class-wp-cbf-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://www.linkedin.com/in/dtquocbao
+ * @since      1.0.0
+ *
+ * @package    Wp_Ssawc
+ * @subpackage Wp_Ssawc/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Wp_Ssawc
+ * @subpackage Wp_Ssawc/includes
+ * @author     Bao Dang <dtquocbao@gmail.com>
+ */
+class Wp_Cbf_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
